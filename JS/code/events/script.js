5fe443ae1457e14fecb769f01d942e8d962e1822
Rajitha Gugloth <guglothrajitha1925@gmail.com>
2025-05-07 21:10:31 +0530

    DOM topic and project updated

---
+++ JS/code/events/script.js
@@ -0,0 +1,36 @@
+document.getElementById('btn').addEventListener('click',display)
+function display()
+{
+    console.log("Hello World")
+}
+function display2() {
+    alert("mouse got hovered")
+}
+document.getElementById('btn').addEventListener('mouseover',change)
+function change()
+{
+    const r1 = Math.round(Math.random()*100);
+    const r2 = Math.round(Math.random()*100);
+    console.log(r1,r2);
+    document.getElementById('btn').style.position = "absolute";
+    document.getElementById('btn').style.top = r1 + "%";
+    document.getElementById('btn').style.top = r2 + "%";
+}
+
+document.addEventListener('keypress',function(e)
+{
+    if (e.key == 'a') 
+    {
+        change()
+    }
+    
+})
+document.getElementById('inp1').addEventListener('input',bgchange)
+
+function bgchange(e)
+{
+    console.log(e.target.value)
+   // document.body.style.background=e.target.value
+   // document.getElementById('clr').addEventListener('change',bgchange)
+}
+//document.getElementById('clr').addEventListener('change',bgchange)
